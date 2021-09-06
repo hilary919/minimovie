d43dabfae5634e5671e810abba28a6c135f55657
hilary919 <1164153512@qq.com>
2021-09-06 18:06:43 +0800

    init

---
+++ data/news.js
@@ -0,0 +1,20 @@
+var newsContent = [{
+    id: 1,
+    text: '车1',
+    desc: '车1的描述'
+  },
+  {
+    id: 2,
+    text: '车2',
+    desc: '车2的描述'
+  },
+  {
+    id: 3,
+    text: '车3',
+    desc: '车3的描述'
+  }
+]
+
+module.exports = {
+  newsContent: newsContent
+}
